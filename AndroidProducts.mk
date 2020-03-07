d3a7d6bf17e534b44eb748a469a6cd548454f3b4
SyberHexen <SyberHexen@gmail.com>
2020-03-07 11:21:47 -0800

    river: havoc-ify
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_river.mk
+    $(LOCAL_DIR)/havoc_river.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_river-eng \
-    lineage_river-user \
-    lineage_river-userdebug
+    havoc_river-eng \
+    havoc_river-user \
+    havoc_river-userdebug
